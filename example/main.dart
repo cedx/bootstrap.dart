4ba03ad763644e8618aac2129f293be5fba81f40
Cédric Belin <cedric@belin.io>
2019-01-19 23:27:06 +0100

    Updated the examples

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,6 @@
 import 'package:bootstrap_bundle/bootstrap_bundle.dart';
 
+/// Prints the version numbers of the available libraries.
 void main() {
   print('Bootstrap: $bootstrapVersion');
   print('jQuery: $jQueryVersion');
