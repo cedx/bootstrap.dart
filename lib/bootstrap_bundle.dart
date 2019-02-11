7a498215d10bf1fcfb19e64e6c9eb52b760c14ac
Cédric Belin <cedric@belin.io>
2019-02-11 22:20:27 +0100

    Bump the version number

--- lib/bootstrap_bundle.dart
+++ lib/bootstrap_bundle.dart
@@ -2,7 +2,7 @@
 library bootstrap_bundle;
 
 /// The version number of the [Bootstrap](https://getbootstrap.com) library.
-const String bootstrapVersion = '4.2.1';
+const String bootstrapVersion = '4.3.0';
 
 /// The version number of the [jQuery](https://jquery.com) library.
 const String jQueryVersion = '3.3.1';
