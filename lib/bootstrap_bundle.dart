505a02152447e9659b81fdc5d1b1933181880277
Cédric Belin <cedric@belin.io>
2019-02-18 18:56:11 +0100

    Updated the documentation

--- lib/bootstrap_bundle.dart
+++ lib/bootstrap_bundle.dart
@@ -2,7 +2,7 @@
 library bootstrap_bundle;
 
 /// The version number of the [Bootstrap](https://getbootstrap.com) library.
-const String bootstrapVersion = '4.3.0';
+const String bootstrapVersion = '4.3.1';
 
 /// The version number of the [jQuery](https://jquery.com) library.
 const String jQueryVersion = '3.3.1';
