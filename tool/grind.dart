2c506c8a5ecea8d692136019ffda77b4ce0a9839
Cédric Belin <cedric@belin.io>
2019-09-13 23:06:45 +0200

    Added a "publish" build task

--- tool/grind.dart
+++ tool/grind.dart
@@ -26,6 +26,10 @@ void fix() => DartFmt.format(existingSourceDirs);
 @Task('Performs the static analysis of source code')
 void lint() => Analyzer.analyze(existingSourceDirs);
 
+@Task('Publishes the package to the registry')
+@Depends(clean, fix)
+void publish() => run('pub', arguments: ['publish', '--force']);
+
 @Task('Upgrades the project to the latest revision')
 void upgrade() {
   run('git', arguments: ['reset', '--hard']);
