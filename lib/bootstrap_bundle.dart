aa538f4a4401b22bb5ab7e7175356d59dcd633b4
Cédric Belin <cedric@belin.io>
2019-11-27 14:31:03 +0100

    Upgraded Bootstrap to version 4.4.0

--- lib/bootstrap_bundle.dart
+++ lib/bootstrap_bundle.dart
@@ -2,7 +2,7 @@
 library bootstrap_bundle;
 
 /// The version number of the [Bootstrap](https://getbootstrap.com) library.
-const String bootstrapVersion = '4.3.1';
+const String bootstrapVersion = '4.4.0';
 
 /// The version number of the [jQuery](https://jquery.com) library.
 const String jQueryVersion = '3.4.1';
